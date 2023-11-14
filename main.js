1ef6529b50c2c614f7abc5ff8370d700f37fcfce
misrar09 <misrar09@gmail.com>
2023-11-14 16:50:59 +0100

    DOM code

--- main.js
+++ main.js
@@ -55,3 +55,53 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const container = document.getElementById("container");
+
+for (let i = 0; i < posts.length; i++) {
+/*     const postId = posts[i].id;
+    const postContent = posts[i].content;
+    const postMedia = posts[i].media;
+    const postAuthorName = posts[i].author.name;
+    const postAuthorImage = posts[i].author.image;
+    const postLikes = posts[i].likes;
+    const postDate = posts[i].created; */
+
+
+    //creating the post in html (DOM )
+    container.innerHTML += ` 
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">Phil Mangione</div>
+                <div class="post-meta__time">4 mesi fa</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+    <div class="post__image">
+        <img src="https://unsplash.it/600/300?image=171" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+            </div>
+        </div> 
+    </div>            
+    </div>`;
+}
+
+
+
+
